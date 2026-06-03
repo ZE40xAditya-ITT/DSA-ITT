d327aa28fe26fa926584240901bd86f6e89423b5
ZE40xAditya-ITT <adityachaturvedi361@gmail.com>
2026-06-03 12:37:10 +0530

    Message
    What to do?

--- Revise.py
+++ Revise.py
@@ -1,8 +1,1 @@
-def largest_element(arr):
-    largest = arr[0]
-    for num in arr:
-        if num > largest:
-            largest = num
-    return largest
-arr = [5,2,8,1,9]
-print(largest_element(arr))
+Revise DSA concepts via practise Questions
