54c6ad64a260ca4d18f1470346da5047df1b8078
ZE40xAditya-ITT <adityachaturvedi361@gmail.com>
2026-06-08 10:10:51 +0530

    Add files via upload

---
+++ LowerBound.py
@@ -0,0 +1,12 @@
+def lower_bound(arr, target):
+    left = 0
+    right = len(arr) - 1
+    answer = len(arr)
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return answer
